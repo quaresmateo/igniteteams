5c1abdce1717d7269e179a70c0b35535e4ec6bc0
quaresmateo <teoian.giovane@gmail.com>
2023-08-17 19:43:17 -0300

    feat: adds back button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,18 @@
-import { Container, Logo } from '@/components/Header/style'
+import { BackButton, BackIcon, Container, Logo } from '@/components/Header/style'
 import logoImg from '@/assets/Logo.png'
 
-export function Header() {
+type Props = {
+  shoeBackButton?: boolean
+}
+
+export function Header({ shoeBackButton = false }: Props) {
   return (
     <Container>
+      {shoeBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImg} />
     </Container>
   )
