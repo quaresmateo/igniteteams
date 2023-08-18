cd4b6441032ee846f432f5c2eb43075f70e37db4
quaresmateo <teoian.giovane@gmail.com>
2023-08-17 21:56:04 -0300

    feat: list components of group card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,18 @@ import { GroupCard } from '@/components/GroupCard'
 import { Header } from '@/components/Header'
 import { Highlight } from '@/components/Highlight'
 import { Container } from '@/screens/Groups/styles'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
 export function Groups() {
+  const exampleGroups = ['Turma 1', 'Turma 2', 'Turma 3', 'Turma 4']
+  const [groups, setGroups] = useState<string[]>(exampleGroups)
+
   return (
     <Container>
       <Header />
       <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
-      <GroupCard title='Turma 01' />
-      <GroupCard title='Turma 02' />
+      <FlatList data={groups} keyExtractor={(item) => item} renderItem={({ item }) => <GroupCard title={item} />} />
     </Container>
   )
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,7 +4,6 @@ import styled from 'styled-components/native'
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-  align-items: 'center';
   justify-content: 'center';
   padding: 20px;
 `
