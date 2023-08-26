c6d36e6a07871d5b426c8e53e0bae6e5ed90776f
quaresmateo <teoian.giovane@gmail.com>
2023-08-25 21:04:43 -0300

    feat: creates player list components

---
+++ src/components/PlayerCard/styles.ts
@@ -0,0 +1,30 @@
+import styled, { css } from 'styled-components/native'
+import { MaterialIcons } from '@expo/vector-icons'
+
+export const Container = styled.View`
+  ${({ theme }) => css`
+    width: 100%;
+    height: 56px;
+    background-color: ${theme.COLORS.GRAY_500};
+    border-radius: 6px;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 16px;
+  `}
+`
+export const Name = styled.Text`
+  ${({ theme }) => css`
+    flex: 1;
+    font-size: ${theme.FONT_SIZE.MD};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.WHITE};
+  `}
+`
+
+export const Icon = styled(MaterialIcons).attrs(({ theme }) => ({
+  size: 24,
+  color: theme.COLORS.GRAY_200
+}))`
+  margin-left: 16px;
+  margin-right: 4px;
+`
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,16 +1,17 @@
-import { Button } from '@/components/Button'
-import { ButtonIcon } from '@/components/ButtonIcon'
-import { Container, Form, HeaderList, NumberOfPlayers } from '@/screens/Players/styles'
-import { Header } from '@/components/Header'
-import { Highlight } from '@/components/Highlight'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 import { Input } from '@/components/Input'
 import { Filter } from '@/components/Filter'
-import { FlatList } from 'react-native'
-import { useState } from 'react'
+import { Header } from '@/components/Header'
+import { Button } from '@/components/Button'
+import { Highlight } from '@/components/Highlight'
+import { PlayerCard } from '@/components/PlayerCard'
+import { ButtonIcon } from '@/components/ButtonIcon'
+import { Container, Form, HeaderList, NumberOfPlayers } from '@/screens/Players/styles'
 
 export function Players() {
   const [team, setTeam] = useState('team a')
-  const [players, setPlayers] = useState<string[]>([])
+  const [players, setPlayers] = useState<string[]>(['Teoian', 'Von', 'Guilherme'])
 
   return (
     <Container>
@@ -31,6 +32,8 @@ export function Players() {
         />
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+
+      <FlatList data={players} keyExtractor={(item) => item} renderItem={({ item }) => <PlayerCard name={item} />} />
 
       {/* <Button label='Remover turma' type='SECONDARY' /> */}
     </Container>
