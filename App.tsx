ac5d8752f936588a32e46a3e4524542582dfd029
quaresmateo <teoian.giovane@gmail.com>
2023-08-25 21:42:44 -0300

    style: reorganizes code

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { Groups } from '@/screens/Groups'
 import theme from '@/theme'
+import { StatusBar } from 'react-native'
+import { Groups } from '@/screens/Groups'
+import { Players } from '@/screens/Players'
+import { NewGroup } from '@/screens/NewGroup'
+import { Loading } from '@/components/Loading'
 import { ThemeProvider } from 'styled-components/native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import { Loading } from '@/components/Loading'
-import { StatusBar } from 'react-native'
-import { NewGroup } from '@/screens/NewGroup'
-import { Players } from '@/screens/Players'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
